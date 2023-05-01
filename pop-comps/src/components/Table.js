b4075f6a6c043bb26bc4059602e748fcb7a565b6
Shantanu Shubham <shantanu0707@gmail.com>
2023-05-01 13:05:58 +0530

    Add Table

--- pop-comps/src/components/Table.js
+++ pop-comps/src/components/Table.js
@@ -1,5 +1,31 @@
-const Table = () => {
-  return <div>This is a Table</div>;
+const Table = ({ data }) => {
+  return (
+    <table className="ui celled table">
+      <thead>
+        <tr>
+          <th>Fruit</th>
+          <th>Color</th>
+          <th>Quantity</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((fruit) => {
+          return (
+            <tr key={fruit.name}>
+              <td>{fruit.name}</td>
+              <td>
+                <div
+                  className="color-box"
+                  style={{ background: fruit.color }}
+                ></div>
+              </td>
+              <td>{fruit.quantity}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 export default Table;
--- pop-comps/src/pages/TablePage.js
+++ pop-comps/src/pages/TablePage.js
@@ -1,9 +1,17 @@
 import Table from "../components/Table";
 
 const TablePage = () => {
+  const data = [
+    { name: "Apple", color: "red", quantity: 10 },
+    { name: "Orange", color: "orange", quantity: 12 },
+    { name: "Guava", color: "#0ed70e", quantity: 8 },
+    { name: "Mango", color: "#ffdb08", quantity: 15 },
+  ];
+
   return (
     <div>
-      <Table />
+      <h1 className="ui header">Table</h1>
+      <Table data={data} />
     </div>
   );
 };
